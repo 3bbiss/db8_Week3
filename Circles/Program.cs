0e7a3be1572a0cd2dc21cc28e70dce29b17856c6
Ali Abbiss <aliabbiss@rocketmortgage.com>
2022-08-15 15:19:14 -0400

    Completed lab and rounded to 2 decimal places

--- Circles/Program.cs
+++ Circles/Program.cs
@@ -1,30 +1,36 @@
 ﻿
+int count = 0;
 double radius;
-string entry;
+string entry, goAgain;
 bool res;
 
-Console.Write("Enter a radius 1.0 to 1000.0: ");
-entry = Console.ReadLine();
-res = double.TryParse(entry, out radius);
-
-while (!res || radius < 1 || radius > 1000)
+do
 {
-    Console.Write("Please enter a valid number for radius 1 to 1000: ");
+    Console.Write("Enter a radius 1.0 to 1000.0: ");
     entry = Console.ReadLine();
     res = double.TryParse(entry, out radius);
-}
+
+    while (!res || radius < 1 || radius > 1000)
+    {
+        Console.Write("Please enter a valid number for radius 1 to 1000: ");
+        entry = Console.ReadLine();
+        res = double.TryParse(entry, out radius);
+    }
+
+    Circle c1 = new Circle(radius);
+
+    Console.WriteLine($"Radius: {c1.getRadius()} -- Area: {c1.CalculateArea()} -- Circumference: {c1.CalculateCircumference()}");
+
+    Console.WriteLine("Do you want to create another circle? Enter y/Y or another key to exit.");
+    goAgain = Console.ReadLine().ToLower();
+    count++;
+} while (goAgain[0] == 'y');
 
 
-// just run it normal. without the class. then integrate the math into
-// the functions below..
-
-
-// so CW the radius, circ, and area of the circule based on the inputted
-// radius.
+Console.WriteLine($"Adios! You made {count} circles!");
 
 class Circle
 {
-
     public double radius;
 
     public double getRadius()
@@ -34,15 +40,15 @@ class Circle
 
     public double CalculateCircumference()
     {
-
+        return Math.Round(2 * Math.PI * radius, 2);
+        //return (2 * Math.PI * radius);
     }
-
 
     public double CalculateArea()
     {
-
+        return Math.Round(Math.PI * radius * radius, 2);
+        //return Math.PI * radius * radius;
     }
-
 
     public Circle(double _radius)
     {
